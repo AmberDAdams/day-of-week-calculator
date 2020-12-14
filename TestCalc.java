d26b874e9c17884c9446d38ec280c581084e818b
Amber <amberdadams1@gmail.com>
2020-12-14 13:00:38 +0000

    add leapyear test

--- TestCalc.java
+++ TestCalc.java
@@ -1,8 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
-
+//import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class TestCalc {
+	
+//	private Calculator calc;
+//	
+//    @Before // setup()
+//    public void before() throws Exception {
+//        System.out.println("Setting it up!");
+//        calc = new Calculator();
+//    }
 
 	@Test
 	void testDayOfWeek() {
@@ -21,7 +29,10 @@ class TestCalc {
 
 	@Test
 	void testIsLeapYear() {
-		fail("Not yet implemented"); // TODO
+		assertTrue(Calculator.isLeapYear(2020));
+		assertFalse(Calculator.isLeapYear(2019));
+		assertTrue(Calculator.isLeapYear(2000));
+		assertFalse(Calculator.isLeapYear(1900));
 	}
 
 	@Test
